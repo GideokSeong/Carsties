ec183ce7409777ad6f4475e5cad356ab356b1938
Gideok Seong <gseong@my.bridgeport.edu>
2024-08-06 15:23:50 -0400

    End of Section 4

---
+++ src/AuctionService/Consumers/AuctionUpdatedConsumer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionUpdatedConsumer
+    {
+        
+    }
+}
