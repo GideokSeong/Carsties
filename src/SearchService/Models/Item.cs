40eeee53d62638da5217f79ea47a918a1f55511c
Gideok Seong <gseong@my.bridgeport.edu>
2024-08-30 15:35:20 -0400

    End of Section 6

--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -8,8 +8,8 @@ public class Item : Entity
     public string Seller { get; set; }
     public string Winner { get; set; }
     public int SoldAmount { get; set; }
-    public int CurrentHighBid { get; set; }
-    public DateTime CreatedAt { get; set; } 
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public DateTime AuctionEnd { get; set; }
     public string Status { get; set; }
